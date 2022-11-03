e308cf46836b06a1b5085939cdfd3505dbeaac75
maroua <chroud.maroua@gmail.com>
2022-11-03 16:24:39 +0100

    component errors ,403,404,500 added

--- front-vanilla/src/init.js
+++ front-vanilla/src/init.js
@@ -1,5 +1,12 @@
 import { initMessages } from "./messages.js";
 import { route } from "./router.js";
+import { error403 } from "./component/error403";
+import { error404 } from "./component/error404";
+import { error500 } from "./component/error500"
+
 
 initMessages();
 route();
+error403();
+error404();
+error500();
