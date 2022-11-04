c723199013164654331e57d0c5cb64d6e7a4a125
ThomasLuton <luton.thomas60@gmail.com>
2022-11-04 10:20:45 +0100

    switch error401 et error403 png and improve 401 et 403 js

--- front-vanilla/src/component/error404.js
+++ front-vanilla/src/component/error404.js
@@ -1,7 +1,12 @@
 function renderError404() {
     const error404Content = `
-    <h1>Oops ! Error 404</h1>
-    <h2>"Page Not Found"</h2>`
+    <div class="container-fluid col-12 col-md-6 my-5 py-5 text-center">
+        <h1>Error 404</h1>
+        <h2>Page Not Found</h2>
+        <img src="sources\\error404.png" class="img-fluid" alt="error404">
+    </div>    
+    `
+
     const main = document.getElementById("main");
     main.innerHTML = error404Content;
 }
