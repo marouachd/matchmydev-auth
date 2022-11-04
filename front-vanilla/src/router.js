29f053e9a00ea0f4de1ef73dcc07cbeb0c40569d
ThomasLuton <luton.thomas60@gmail.com>
2022-11-04 10:57:42 +0100

    add on router error 401 403 500

--- front-vanilla/src/router.js
+++ front-vanilla/src/router.js
@@ -1,6 +1,8 @@
 import { renderSignUp } from "./component/signUp.js";
 import { renderError401 } from "./component/error401.js";
+import { renderError403 } from "./component/error403.js";
 import { renderError404 } from "./component/error404.js";
+import { renderError500 } from "./component/error500.js";
 
 window.addEventListener('hashchange', (event) => route(event));
 
@@ -10,8 +12,12 @@ function route() {
     if (hash == "" || hash == "index.html") {
         renderSignUp();
     }
-    else if (hash == "unauthaurized") {
+    else if (hash == "#/unauthaurized") {
         renderError401();
+    } else if (hash == "#/forbiden") {
+        renderError403();
+    } else if (hash == "#/serverdown") {
+        renderError500();
     } else {
         renderError404();
     }
