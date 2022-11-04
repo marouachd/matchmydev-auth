1855f24c0d23314197fc30c80181b541b35bce64
maroua <chroud.maroua@gmail.com>
2022-11-04 10:21:29 +0100

    error 403 image added

--- front-vanilla/src/component/error403.js
+++ front-vanilla/src/component/error403.js
@@ -1,7 +1,10 @@
 function renderError403() {
     const error403Content = `
-    <h1>Oops ! Error 403</h1>
-    <h2>You already have signed-up with given identifier and email.</h2>
+    <div class="container-xl my-5 pt-5 text-center">
+        <h1>Oops ! Error 403</h1>
+        <h2>Forbidden Page</h2>
+        <img src="sources/error403.png" class="img-fluid" alt="403 error">
+    </div>
     `
     const main = document.getElementById("main");
     main.innerHTML = error403Content;
