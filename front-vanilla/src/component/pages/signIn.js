58fcfb6b0446d532c65e66c8206050ea4904469b
ThomasLuton <luton.thomas60@gmail.com>
2022-11-07 17:02:20 +0100

    fix signIn.js (bootstrap, and import)

--- front-vanilla/src/component/pages/signIn.js
+++ front-vanilla/src/component/pages/signIn.js
@@ -1,17 +1,17 @@
 import { messages } from "../../messages.js";
-import { signInForm } from "../patterns/signInForm.js";
+import { renderSignInForm } from "../patterns/signInForm.js";
 
 
 function renderSignIn() {
     const contentSignIn = `
-    <div class="container-xl my-5 pt-5">
+    <div class="container-xl col-md-6 col-12 mx-auto my-5 pt-5">
             <h1>${messages.signIn.h1}</h1>
             <section id="section1"></section>
         </div>
     `
     const main = document.getElementById("main");
     main.innerHTML = contentSignIn;
-    signInForm("section1");
+    renderSignInForm("section1");
 
 }
 export { renderSignIn };
