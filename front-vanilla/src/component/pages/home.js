48e389d49cd3388a4de2f843d8b512e5a65e87f4
JulietteMadi <juliette.madinier@gmail.com>
2022-11-10 12:15:26 +0100

    Updating for merge

--- front-vanilla/src/component/pages/home.js
+++ front-vanilla/src/component/pages/home.js
@@ -7,11 +7,11 @@ function renderHome() {
     <div class="container-xl my-5 pt-5">
         <h1>${messages.home.h1}</h1>
         <div class="row">
-            <div class="col-md my-5 mx-2">
+            <div class="col-md my-5 mx-auto">
                 <h2>${messages.signIn.h1}</h2>
                 <section id="section1"></section>
             </div>
-            <div class="col-md my-5 mx-2">
+            <div class="col-md my-5 mx-auto">
                 <h2>${messages.signUp.h1}</h2>
                 <section id="section2"></section>
             </div>
