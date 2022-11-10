362d4a05796b26aeeee808e6aeff90ed4ea5efbd
ThomasLuton <luton.thomas60@gmail.com>
2022-11-10 16:26:56 +0100

    "#63 #64 fix SignInComponent don't forget the =>()

--- front-vanilla/src/component/pages/SignInComponent.js
+++ front-vanilla/src/component/pages/SignInComponent.js
@@ -12,7 +12,7 @@ export default class SignInComponent extends BaseComponent {
         <div class="container-xl col-md-6 col-12 mx-auto my-5 pt-5">
             <h1>${this.msg.signIn.h1}</h1>
             <section class="col-md-6 col-12 mx-auto" id="section1">
-            ${FormsContent.signInForm}
+            ${FormsContent.signInForm()}
             </section>
         </div>
         `
