40a71fa58ae8a44d4245b976cf7847e078fb5402
JulietteMadi <juliette.madinier@gmail.com>
2022-11-18 15:26:47 +0100

    first commit api

---
+++ auth-api/src/main/java/co/simplon/matchmydev/auth/AuthApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.matchmydev.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApiApplication.class, args);
+	}
+
+}
