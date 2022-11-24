69edcb70a8c2bf27121114f460786299e116c9d1
ThomasLuton <luton.thomas60@gmail.com>
2022-11-24 15:34:12 +0100

    conflit generator

--- front-vanilla/src/component/pages/AccountsListComponent.js
+++ front-vanilla/src/component/pages/AccountsListComponent.js
@@ -9,34 +9,38 @@ export default class AccountsListComponent extends BaseIterableComponent {
         }
         );
     }
-    template(element) {
+    template() {
         return `
         <h1>Accounts list</h1>
         <div class="table-responsive my-5">
-            <table id="accountslist" class="table table-bordered ">
-                <thead class="head-table">
-                    <tr>
-                        <th scope="col">Internal identifier</th>
-                        <th scope="col">Internal email</th>
-                        <th scope="col">Password</th>
-                        <th scope="col">Creation date</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>${element.internalIdentifier}</td>
-                        <td> ${element.internalEmail}</td>
-                        <td> ${element.password}</td>
-                        <td> ${element.createdAt} </td>
-                        <td class="text-center">
-                        <a href=""><i class="bi bi-circle-fill text-${element.active ? "success" : "danger"}"</i></a>
-                        </td>
-                    </tr>
-                </tbody>
+            <table class="table table-bordered ">
+            <thead class="head-table">
+            <tr>
+                <th scope="col">Internal identifier</th>
+                <th scope="col">Internal email</th>
+                <th scope="col">Password</th>
+                <th scope="col">Creation date</th>
+                <th scope="col">Status</th>
+            </tr>
+        </thead>
+        <tbody id="accountslist">
+            
+        </tbody>
             </table>
         </div>
-        
         `
     }
-};
+    elementsTemplate(element) {
+
+        return `
+        <tr>
+            <td>${element.internalIdentifier}</td>
+            <td> ${element.internalEmail}</td>
+            <td> ${element.password}</td>
+            <td> ${element.createdAt} </td>
+            <td class="text-center">
+            <a href=""><i class="bi bi-circle-fill text-${element.active ? "success" : "danger"}"</i></a>
+            </td>
+        </tr>`
+    }
+}
