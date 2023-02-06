7de498653cdee8d11f5b89487b3c2e94f5fa2da4
ThomasLuton <luton.thomas60@gmail.com>
2023-02-06 17:27:19 +0100

    ...

--- front-vue/MatchMyDev-front/src/components/Admin.vue
+++ front-vue/MatchMyDev-front/src/components/Admin.vue
@@ -1,7 +1,7 @@
 <script setup>
-import AccountList from './commons/AccountList.vue';
+// import AccountList from './commons/AccountList.vue';
 </script>
 
 <template>
-    <AccountList></AccountList>
+    <!-- <AccountList></AccountList> -->
 </template>
