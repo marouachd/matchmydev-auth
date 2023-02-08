d52cc91f02a564c897fe28084aa0dcdbc9741a70
ThomasLuton <luton.thomas60@gmail.com>
2023-02-08 12:49:43 +0100

    fix conflict

--- auth-api/src/main/java/co/simplon/matchmydev/auth/controllers/AuthController.java
+++ auth-api/src/main/java/co/simplon/matchmydev/auth/controllers/AuthController.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,39 +13,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import co.simplon.matchmydev.auth.dtos.UserAccountCreate;
 import co.simplon.matchmydev.auth.dtos.UserAccountView;
 import co.simplon.matchmydev.auth.services.UserAccountService;
 
 @RestController
-@RequestMapping("/useraccount")
+@RequestMapping("/user-accounts")
 @CrossOrigin
 public class AuthController {
-	
-	private UserAccountService service;
-	
-	public AuthController (UserAccountService service) {
-		this.service = service;
-		
-	}
-	
+
+    private UserAccountService service;
+
+    public AuthController(UserAccountService service) {
+	this.service = service;
+
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void create(@Valid @RequestBody UserAccountCreate inputs) {
 
-    		service.create(inputs);
-     System.out.println("juliette est gentille :D");
+	service.create(inputs);
+	System.out.println("juliette est gentille :D");
 
     }
 
     @GetMapping
     public Collection<UserAccountView> getAll() {
-    	System.out.println("juliette est gentille :D");
-		return service.getAll();
-		 
+	System.out.println("juliette est gentille :D");
+	return service.getAll();
 
     }
-    
+
 }
