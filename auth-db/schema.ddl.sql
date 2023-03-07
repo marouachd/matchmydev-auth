bde0027cc0d1881faad93bff5c5e11770f9c838c
JulietteMadi <juliette.madinier@gmail.com>
2023-03-07 15:06:29 +0100

    Added role and joined columns

--- auth-db/schema.ddl.sql
+++ auth-db/schema.ddl.sql
@@ -1,4 +1,10 @@
 DROP TABLE IF EXISTS user_accounts;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE roles (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR (20) UNIQUE NOT NULL
+);
 
 CREATE TABLE user_accounts (
 
@@ -7,6 +13,10 @@ CREATE TABLE user_accounts (
 	internal_email VARCHAR (255) UNIQUE NOT NULL,
 	password CHAR(60) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-	active BOOLEAN NOT NULL
+	active BOOLEAN NOT NULL,
+	role_id INTEGER,
+	CONSTRAINT fk_role_id
+		FOREIGN KEY (role_id)
+		REFERENCES roles(id)
 	
 );
